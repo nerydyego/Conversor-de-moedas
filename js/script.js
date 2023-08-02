067248e633baa88313579fd73e838ba2aa39a578
nerydyego <dyegonery@gmail.com>
2023-08-02 11:42:39 -0300

    Finalização - Projeto

--- js/script.js
+++ js/script.js
@@ -17,8 +17,6 @@ fetch(`https://${host}/currencies`)
     const entries = Object.entries(data);
     const select1 = document.getElementById("moeda1");
     const select2 = document.getElementById("moeda2");
-    console.log(entries)
-    //alert(`10 GBP = ${data.rates.USD} USD`);
     for( i = 0; i < entries.length; i++){
         select1.innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}-${entries[i][1]}</option>`;
         select2.innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}-${entries[i][1]}</option>`;
@@ -29,18 +27,21 @@ fetch(`https://${host}/currencies`)
     var input_valor = document.getElementById('input_moeda').value;
     var select1Value = document.getElementById('moeda1').value;
     var select2Value = document.getElementById('moeda2').value;
+    var output_moeda = document.getElementById('output_moeda');
+    output_moeda.textContent = '';
+    
 
     if(select1Value != select2Value){
         const host = 'api.frankfurter.app';
-        fetch(`https://${host}/latest?amount=${input_valor}&from=${select1Value.value}&to=${select2Value.value}`)
+        fetch(`https://${host}/latest?amount=${input_valor}&from=${select1Value}&to=${select2Value}`)
             .then((val) => val.json())
             .then((val) => {
-            output_moeda.value = Object.values(val.rates)[0];
-            //alert(`10 GBP = ${data.rates.USD} USD`);
+            output_moeda.value = Object.values(val.rates);
         });
 
     }else{
         alert("Selecione valor de moeda diferente!")
     }
+
   }
 
